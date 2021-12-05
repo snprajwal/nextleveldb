d4f4cde5b9f8748d941470ba9c59097d2ba6be6e
Prajwal S N <prajwalnadig21@gmail.com>
2021-12-05 23:35:38 +0530

    feat(cli): added init logo

---
+++ src/NextLevelDB.java
@@ -0,0 +1,11 @@
+package nextleveldb;
+
+public class NextLevelDB {
+	public static void main(String[] args) {
+		Cli cli = new Cli();
+		cli.printLogo();
+		// while (true) {
+		// 	cli.run();
+		// }
+	}
+}
